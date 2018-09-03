d0a0d96a58770105693ae971395b956822fea374
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-09-03 01:07:59 -0300

    Arquivos de casos de estudo agora criam arquivos txt para resultados

--- live_journal.cpp
+++ live_journal.cpp
@@ -22,13 +22,16 @@ int main(){
 
     FILE* pFile;
 
+	FILE* pFileOut;
+
     pFile = fopen("live_journal.txt", "r");
+	pFileOut = fopen("resultados_as_graph_matriz.txt", "a");
 
 	long inicio, fim, acum_bfs = 0, acum_dfs = 0;
 
 	double duracao;
 
-	printf("Análise do grafo com Lista de Adjacência:\n\n");
+	fprintf(pFileOut, "Análise do grafo com Lista de Adjacência:\n\n");
 	
 	inicio = getMicrotime();
 
@@ -38,7 +41,7 @@ int main(){
 
 	duracao = (fim-inicio) / 1000;
 
-	printf("Construindo Lista de Adjacência (%.0lf ms)\n\n", duracao);
+	fprintf(pFileOut, "Construindo Lista de Adjacência (%.0lf ms)\n\n", duracao);
     
 	for(i = 0; i<LEITURAS; i++){
 		inicio = getMicrotime();
@@ -54,31 +57,27 @@ int main(){
 		acum_dfs += duracao;
 	}
 
-	printf("Tempo Médio (%d observações):\n\tBFS: %ld ms\n\tDFS: %ld ms\n\n",LEITURAS, acum_bfs/LEITURAS, acum_dfs/LEITURAS);
+	fprintf(pFileOut, "Tempo Médio (%d observações):\n\tBFS: %ld ms\n\tDFS: %ld ms\n\n",LEITURAS, acum_bfs/LEITURAS, acum_dfs/LEITURAS);
 
 	for(int i=0;i<5;i++){
 		grafo.BFS(i);
-		printf("BFS a partir de %d:\n",i+1);
+		fprintf(pFileOut, "BFS a partir de %d:\n",i+1);
 		for(int j = 9;j<50;j+=10){
-			printf("O pai de %d é %d\n",j+1,grafo.getVertices()[j]->getPai()+1);
+			fprintf(pFileOut, "O pai de %d é %d\n",j+1,grafo.getVertices()[j]->getPai()+1);
 		}
 		grafo.DFS(i);
-		printf("\nDFS a partir de %d:\n",i+1);
+		fprintf(pFileOut, "\nDFS a partir de %d:\n",i+1);
 		for(int j = 9;j<50;j+=10){
-			printf("O pai de %d é %d\n",j+1,grafo.getVertices()[j]->getPai()+1);
+			fprintf(pFileOut, "O pai de %d é %d\n",j+1,grafo.getVertices()[j]->getPai()+1);
 		}
-		printf("\n");
+		fprintf(pFileOut, "\n");
 	}
 
 	Lista** componentes = grafo.analiseComponentesConexos();
 	int quantidade = grafo.componentesConexos().size();
 	int maior = componentes[0]->getTamanho();
 	int menor = componentes[quantidade-1]->getTamanho();
-	printf("O grafo possui %d componentes conexas.\n A maior tem tamanho %d.\n A menor tem tamanho %d\n\n",quantidade,maior,menor);
+	fprintf(pFileOut, "O grafo possui %d componentes conexas.\n A maior tem tamanho %d.\n A menor tem tamanho %d.\n\n",quantidade,maior,menor);
 
-	printf("Grau mínimo: %d\nGrau máximo: %d\nGrau médio: %f\nMediana do grau: %f\n\n",grafo.getGrauMin(),grafo.getGrauMax(),grafo.getGrauMedio(),grafo.getGrauMediano(	));
-    
-	//printf("Diâmetro do grafo(-1 se o grafo for disconexo): %d\n",grafo.diametro());
-	// Apresentação dos resultados
-    //printf("Resultados:\n\tVértices: %u\n\tArestas: %u\n\tMediana: %.2f\n", lista.getNVertices(), lista.getNArestas(), lista.getGrauMediano());
+	fprintf(pFileOut, "Número de vértices: %d\nNúmero de Arestas: %d\nGrau mínimo: %d\nGrau máximo: %d\nGrau médio: %.2f\nMediana do grau: %.2f\n\n", grafo.getNVertices(), grafo.getNArestas(), grafo.getGrauMin(),grafo.getGrauMax(),grafo.getGrauMedio(),grafo.getGrauMediano(	));
 }
