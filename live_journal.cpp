47ba82de9c7acab7def8283d965d37c35c0cab8b
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-09-03 11:51:16 -0300

    Corrigindo main do live_journal

--- live_journal.cpp
+++ live_journal.cpp
@@ -16,7 +16,7 @@ long getMicrotime(){
 
 int main(){
 
-	const int LEITURAS = 1000;
+	const int LEITURAS = 10;
 
 	int i;
 
@@ -25,7 +25,7 @@ int main(){
 	FILE* pFileOut;
 
     pFile = fopen("live_journal.txt", "r");
-	pFileOut = fopen("resultados_as_graph_matriz.txt", "a");
+	pFileOut = fopen("resultados_live_journal.txt", "a");
 
 	long inicio, fim, acum_bfs = 0, acum_dfs = 0;
 
@@ -59,7 +59,7 @@ int main(){
 
 	fprintf(pFileOut, "Tempo Médio (%d observações):\n\tBFS: %ld ms\n\tDFS: %ld ms\n\n",LEITURAS, acum_bfs/LEITURAS, acum_dfs/LEITURAS);
 
-	for(int i=0;i<5;i++){
+	/* for(int i=0;i<5;i++){
 		grafo.BFS(i);
 		fprintf(pFileOut, "BFS a partir de %d:\n",i+1);
 		for(int j = 9;j<50;j+=10){
@@ -80,4 +80,4 @@ int main(){
 	fprintf(pFileOut, "O grafo possui %d componentes conexas.\n A maior tem tamanho %d.\n A menor tem tamanho %d.\n\n",quantidade,maior,menor);
 
 	fprintf(pFileOut, "Número de vértices: %d\nNúmero de Arestas: %d\nGrau mínimo: %d\nGrau máximo: %d\nGrau médio: %.2f\nMediana do grau: %.2f\n\n", grafo.getNVertices(), grafo.getNArestas(), grafo.getGrauMin(),grafo.getGrauMax(),grafo.getGrauMedio(),grafo.getGrauMediano(	));
-}
+ */}
