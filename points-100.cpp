b75a824dd0de5c7ac56a6c5b69cb4e1eef08955c
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2018-11-25 13:06:46 -0200

    Cria arquivos de teste

---
+++ points-100.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "grafo.h"
+#include "tupla.h"
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+
+int main(){
+
+    FILE* pFileIn;
+    FILE* pFileOut;
+
+    Tupla<int*, float> t;
+
+    chrono::high_resolution_clock::time_point inicio, fim;
+    chrono::duration<double> duracao;
+
+    int n;
+
+    pFileIn = fopen("points-100.txt","r");
+    pFileOut = fopen("temposTSP.txt","a");
+
+    n = 100;
+
+    fprintf(pFileOut, "Tempos para %d pontos:\t\n", n);
+
+    inicio = chrono::high_resolution_clock::now();
+    MatrizAdjacencias G(pFileIn);
+    fim = chrono::high_resolution_clock::now();
+
+    duracao = chrono::duration_cast<chrono::duration<double>>(fim-inicio);
+    fprintf(pFileOut, "Tempo construtor: %lf ms\n", duracao);
+
+    inicio = chrono::high_resolution_clock::now();
+    t = G.TSP_lexicografico();
+    fim = chrono::high_resolution_clock::now();
+    
+    duracao = chrono::duration_cast<chrono::duration<double>>(fim-inicio);
+    fprintf(pFileOut, "Método lexicográfico: %lf ms\n", duracao);
+    free(t.elem1);
+    
+    inicio = chrono::high_resolution_clock::now();
+    t = G.TSP_vizinhosMaisProximos();
+    fim = chrono::high_resolution_clock::now();
+    
+    duracao = chrono::duration_cast<chrono::duration<double>>(fim-inicio);
+    fprintf(pFileOut, "Método vizinhos mais próximos: %lf ms\n\n", duracao);
+    free(t.elem1);
+    
+    return 0;
+}
